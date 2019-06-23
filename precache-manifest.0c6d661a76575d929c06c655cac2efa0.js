e0f76f067cfde34c612728dce99055a66dc4d6f7
Sumit <sumit2911997@gmail.com>
2019-06-24 02:06:27 +0530

    Updates

---
+++ precache-manifest.0c6d661a76575d929c06c655cac2efa0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "273a7157d00508f94209721ae18abc60",
+    "url": "/credit-app/index.html"
+  },
+  {
+    "revision": "4ace151bfdff9033452e",
+    "url": "/credit-app/static/css/main.c17a2ab8.chunk.css"
+  },
+  {
+    "revision": "25cfba78603c4f03f4c1",
+    "url": "/credit-app/static/js/2.1884de0e.chunk.js"
+  },
+  {
+    "revision": "4ace151bfdff9033452e",
+    "url": "/credit-app/static/js/main.12326d1a.chunk.js"
+  },
+  {
+    "revision": "ea09da5eb1d5955c418c",
+    "url": "/credit-app/static/js/runtime~main.fedc7451.js"
+  }
+]);
