461d544863687a0f19cee0c9436d9980537d83a7
Sumit <sumit2911997@gmail.com>
2019-06-24 13:56:58 +0530

    enhanced navigation support

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import AllUsers from './AllUsers.js';
+import Transactions from './Transactions.js';
 
 class HomePage extends React.Component {
     render() {
         return (
             <>
-            <button onClick = {() => this.props.onClick(<AllUsers goBack = {() => {this.props.goBack()}}/>)}>All Users</button>
-            <button>Transactions</button>
+            <button 
+            onClick = {() => this.props.onClick(<AllUsers 
+                                                goBack = {() => {this.props.goBack()}}
+                                                onClick = {(page) => {this.props.onClick(page)}}
+                                                />)
+                        }
+            >
+                Show All Users
+            </button>
+            <button
+            onClick = {() => this.props.onClick(<Transactions 
+                                                goBack = {() => {this.props.goBack()}} 
+                                                />)
+                        }
+            >
+                Show All Transactions
+            </button>
             </>
         );
     }
